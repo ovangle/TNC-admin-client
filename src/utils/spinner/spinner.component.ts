f8ad3420ec9689d6fe6cef46b0d321c655c3925d
Thomas Stephenson <ovangle@gmail.com>
2016-06-14 23:51:49 +1000

    Add a progress spinner component

---
+++ src/utils/spinner/spinner.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, Output, ChangeDetectionStrategy, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    selector: 'spinner',
+    template: `
+        <div class="loader"
+             [style.font-size]="size || 'inherit'"
+             [style.color]="color || _defaultColor"></div>
+    `,
+    styles: [`
+    :host {
+        display: inline-block; 
+    }    
+    `],
+    styleUrls: [
+        //TODO: Research reslative url
+        'src/utils/spinner/spinner.component.css'
+    ],
+    encapsulation: ViewEncapsulation.Native,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Spinner {
+    _defaultColor = 'rgb(255,255,255,0.2)'
+
+    // A css indicator for the size. If not provided, defaults to
+    // the current font size.
+    @Input() size: string;
+
+    // A css color indicator
+    @Input() color: string;
+}
+
