dc9664ada0852978da176dee944650290121cef6
Thomas Stephenson <ovangle@gmail.com>
2016-08-01 12:35:32 +1000

    Refactor term display component
    - Display term type
    - Use details_display css

--- src/member/term/term_display.component.ts
+++ src/member/term/term_display.component.ts
@@ -2,34 +2,57 @@ import {
     Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsulation
 } from '@angular/core';
 
+import {OrElsePipe} from '../../utils/pipes';
+
+import {MEMBER_TERM_TYPE_VALUES} from './term_type.model';
 import {MemberTerm} from './term.model';
 
 @Component({
     selector: 'member-term-display',
     template: `
-        <div class="alert alert-warning">
-            <p><strong>Member since</strong> {{term?.joined | date }}</p>
-            <p><strong>Renewed</strong> {{term?.renewed | date}}</p>    
-            <p><strong>Expires</strong> {{term?.expires | date}}</p>
-            
-            <p class="clearfix">
-                <button class="btn btn-default pull-right" 
-                        (click)="renew.emit(true)">
-                    <i class="fa fa-pencil-square-o"></i> Renew
-                </button>
-            </p>
+    <div class="alert alert-warning">
+        <div class="member-type"><strong>{{termType}} member</strong></div>
+
+        <ul class="list-unstyled">
+            <li class="clearfix">
+                <span class="display-label col-sm-4">Joined</span>
+                <span class="display-value col-sm-8">{{term.joined | date}}</span>
+            </li>
+            <li class="clearfix">
+                <span class="display-label col-sm-4">Renewed</span>
+                <span class="display-value col-sm-8">{{term.renewed | date | orElse: 'Never'}}</span>
+            </li>
+            <li class="clearfix">
+                <span class="display-label col-sm-4">Expires</span> 
+                <span class="display-value col-sm-8">{{term.expires | date}}</span>
+            </li>
+        </ul>   
+        <div class="clearfix term-actions">
+            <button class="btn btn-default btn-renew pull-right" 
+                    (click)="renew.emit(true)">
+                <i class="fa fa-pencil-square-o"></i> Renew
+            </button>
         </div>
+    </div>    
     `,
-    directives: [],
+    pipes: [OrElsePipe],
     styles: [`
     :host {
         display: block;
     }    
+    .member-type {
+        margin-bottom: 10px; 
+    }    
+    .term-actions {
+        margin-top: 5px; 
+    }    
+       
     `],
     styleUrls: [
         'assets/css/font-awesome.css',
         'assets/css/bootstrap.css',
-        'assets/css/flex.css'
+        'assets/css/flex.css',
+        'assets/css/details_display.css'
     ],
     encapsulation: ViewEncapsulation.Native,
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -37,5 +60,9 @@ import {MemberTerm} from './term.model';
 export class MemberTermDisplay {
     @Input() term: MemberTerm;
     @Output() renew = new EventEmitter<any>();
+
+    get termType(): string {
+        return MEMBER_TERM_TYPE_VALUES.get(this.term.type);
+    }
 
 }
