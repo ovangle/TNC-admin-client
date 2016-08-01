37710080a1438cea42ee39d9abd739c9260cf87f
Thomas Stephenson <ovangle@gmail.com>
2016-08-01 12:33:38 +1000

    Removed old details page implementation

--- src/member/basic.ts
+++ src/member/basic.ts
@@ -5,5 +5,3 @@ export * from './basic/name';
 export * from './basic/residential_status';
 export * from './basic/contact';
 export * from './basic/income';
-
-export {MemberBasicDetails} from './basic/member_basic_details.page';
--- src/member/member.routes.ts
+++ src/member/member.routes.ts
@@ -1,13 +1,11 @@
 import {RouterConfig} from '@angular/router';
 
 import {MemberSearchPage} from './search_page.component';
-import {MemberDetailsComponent} from './details.component';
 import {MemberDetailsPage} from './details_page.component';
 import {MemberSignupPage} from './signup_page.component';
 import {MemberRenewalPage} from './renewal_page.component';
 
 import {MEMBER_DETAIL_ROUTES} from './details/member_details.routes';
-
 
 export const MEMBER_ROUTES: RouterConfig = [
     {
@@ -16,7 +14,6 @@ export const MEMBER_ROUTES: RouterConfig = [
             {path: '', component: MemberSearchPage},
             {path: 'signup', component: MemberSignupPage},
             {path: 'renew/:id', component: MemberRenewalPage},
-            {path: ':id', component: MemberDetailsComponent, children: MEMBER_DETAIL_ROUTES},
             {path: 'details/:id', component: MemberDetailsPage, children: MEMBER_DETAIL_ROUTES}
         ]
     }
