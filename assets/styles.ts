d9b1ec17a052c89002d2f0f6b74c7478d29c6726
Thomas Stephenson <ovangle@gmail.com>
2017-07-10 04:56:57 +1000

    Bundle styles with webpack

---
+++ assets/styles.ts
@@ -0,0 +1,11 @@
+// Global styles
+
+import 'bootstrap/dist/css/bootstrap';
+import 'font-awesome/css/font-awesome';
+
+import './css/details_display';
+import './css/flex';
+import './css/modals';
+import './css/positioning';
+import './css/search_page';
+import './css/search_result_table';
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -10,6 +10,7 @@ module.exports = {
     entry: {
         'polyfills': './assets/polyfills.ts',
         'vendor': './assets/vendor.ts',
+        'styles': './assets/styles.ts',
         'main': './src/main.ts'
     },
     resolve: {
@@ -38,26 +39,32 @@ module.exports = {
                     minimize: false
                 }
             },
+            // Load files in the src directory directly into the module bundles
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            // The following two loaders target the font-awesome fonts
             // the url-loader uses DataUrls.
             // the file-loader emits files.
             {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
                 // loader: "url?limit=10000"
                 loader: "url-loader"
             },
             {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file"
-            },
-            // Load files in the src directory directly into the module bundles
-            {
-                test: /\.css$/,
-                loader: 'raw-loader'
+                test: /\.(ttf|eot|svg)(\?v=[0-9]+\.[0-9]+\.[0-9]+)?$/,
+                loader: "file-loader"
             }
         ]
     },
     plugins: [
+        new ExtractTextPlugin('styles.css'),
+
         new webpack.ContextReplacementPlugin(
           /angular(\\|\/)core(\\|\/)@angular/,
           path.resolve(__dirname, '../src')
